a8872a12b164fd5cfcaf4003a9ae1d1248fb4430
graham.north@viewdeck.com <graham.north@laptop-26.remote.viewdeck.com>
2020-02-26 10:25:49 +0000

    Use configuration file to define pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-echo "This is a test pipeline"
+configuration="$1"
 
-sudo docker run -v $PWD:/host -i --entrypoint=/host/terraform.sh hashicorp/terraform
+echo "configuration=$configuration"
+
+. "$configuration"
+
+echo "entrypoint=$entrypoint"
+echo "container=$container"
+echo "hostmount=$hostmount"
+
+[[ -z "$configuration" ]] && exit
+[[ -z "$entrypoint" ]] && exit
+[[ -z "$container" ]] && exit
+[[ -z "$hostmount" ]] && exit
+
+docker run -i \
+    -v "$PWD:$hostmount" \
+    --entrypoint="$hostmount/$entrypoint" \
+    "$container"
