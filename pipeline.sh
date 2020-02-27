bd2234ef25c161ef4433ffb787d8377622d6eee0
graham.north@viewdeck.com <graham.north@laptop-26.remote.viewdeck.com>
2020-02-27 13:48:00 +0000

    Refine CIFS code

--- pipeline.sh
+++ pipeline.sh
@@ -17,21 +17,25 @@ echo "hostshare=$hostshare"
 [[ -z "$hostmount" ]] && exit
 [[ -z "$hostshare" ]] && exit
 
-sudo mkdir /mnt/peith7teashoo7ab
-if [ ! -d "/etc/smbcredentials" ]; then
-sudo mkdir /etc/smbcredentials
-fi
-if [ ! -f "/etc/smbcredentials/peith7teashoo7ab.cred" ]; then
-    sudo bash -c 'echo "username=peith7teashoo7ab" >> /etc/smbcredentials/peith7teashoo7ab.cred'
-    sudo bash -c 'echo "password=3eQgk6CuzLlTKyyyeeb/UWuBj5TdoIdEkJ7L4lNImb2sn9++Blf/WAQc4JpNZoynJYZt6C4EjfQ4uvAFZMdBkA==" >> /etc/smbcredentials/peith7teashoo7ab.cred'
-fi
-sudo chmod 600 /etc/smbcredentials/peith7teashoo7ab.cred
+cifsshare="//peith7teashoo7ab.file.core.windows.net/pipeline"
+cifsmount="/mnt/peith7teashoo7ab"
+cifsoptions="vers=3.0,credentials=$cifscred,dir_mode=0777,file_mode=0777,serverino"
+cifsconfig="/etc/smbcredentials"
+cifscred="$cifsconfig/peith7teashoo7ab.cred"
 
-sudo bash -c 'echo "//peith7teashoo7ab.file.core.windows.net/pipeline /mnt/peith7teashoo7ab cifs nofail,vers=3.0,credentials=/etc/smbcredentials/peith7teashoo7ab.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab'
-sudo mount -t cifs //peith7teashoo7ab.file.core.windows.net/pipeline /mnt/peith7teashoo7ab -o vers=3.0,credentials=/etc/smbcredentials/peith7teashoo7ab.cred,dir_mode=0777,file_mode=0777,serverino
+sudo mkdir -vp "$cifsmount"
+sudo mkdir -vp "$cifsconfig"
+
+sudo bash -c "echo \"username=peith7teashoo7ab\" > $cifscred"
+sudo bash -c "echo \"password=3eQgk6CuzLlTKyyyeeb/UWuBj5TdoIdEkJ7L4lNImb2sn9++Blf/WAQc4JpNZoynJYZt6C4EjfQ4uvAFZMdBkA==\" >> $cifscred"
+
+sudo chmod -v 600 "$cifscred"
+
+sudo bash -c "echo \"$cifsshare $cifsmount cifs nofail,$cifsoptions\" >> /etc/fstab"
+sudo mount -vt cifs "$cifsshare" "$cifsmount" -o "$cifsoptions"
 
 docker run -i \
     -v "$PWD:$hostmount" \
-    -v "/mnt/peith7teashoo7ab:$hostshare" \
+    -v "$cifsmount:$hostshare" \
     --entrypoint="$hostmount/$entrypoint" \
     "$container"
